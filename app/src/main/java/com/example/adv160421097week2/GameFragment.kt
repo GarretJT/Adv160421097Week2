eb7c4c74a3a4c068b1abf6857e6d562f7a22bfe2
GarretJT <garrettanzil@gmail.com>
2024-03-06 00:55:58 +0700

    init

---
+++ app/src/main/java/com/example/adv160421097week2/GameFragment.kt
@@ -0,0 +1,61 @@
+package com.example.adv160421097week2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.adv160421097week2.databinding.FragmentGameBinding
+import kotlin.random.Random
+
+class GameFragment : Fragment() {
+    private lateinit var binding: FragmentGameBinding
+
+    private var num1: Int = 0
+    private var num2: Int = 0
+    public var result = 0
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentGameBinding.inflate(inflater,container, false)
+        return binding.root
+    }
+
+    private fun generateRandomNumbers() {
+        num1 = Random.nextInt(101) // Random number between 0 and 100
+        num2 = Random.nextInt(101) // Random number between 0 and 100
+    }
+
+    private fun displayQuestion() {
+        binding.txtMath.text = "${num1} + ${num2}"
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            val playerName =
+                GameFragmentArgs.fromBundle(requireArguments()).playerName
+            binding.txtTurn.text = "$playerName's Turn"
+        }
+
+        generateRandomNumbers();
+        displayQuestion();
+
+        binding.btnSubmit.setOnClickListener {
+            val answer = binding.txtAnswer.text.toString()
+            if (answer == (num1 + num2).toString()) {
+                result += 1;
+                generateRandomNumbers();
+                displayQuestion();
+            } else {
+                val action = GameFragmentDirections.actionGameFragmentToResultFragment(result)
+                Navigation.findNavController(it).navigate(action)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/adv160421097week2/ResultFragment.kt
@@ -0,0 +1,46 @@
+package com.example.adv160421097week2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.adv160421097week2.databinding.FragmentResultBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ResultFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ResultFragment : Fragment() {
+    private lateinit var binding: FragmentResultBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentResultBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnBackToMain.setOnClickListener {
+            val action = ResultFragmentDirections.actionResultFragmentToMainFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+        arguments?.let {
+            val score =
+                ResultFragmentArgs.fromBundle(requireArguments()).result
+            binding.score.text = "Your score is $score"
+        }
+
+    }
+}
