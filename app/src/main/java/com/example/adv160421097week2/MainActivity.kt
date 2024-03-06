6fd39d01ca79dec456be5e161117178f3bc1a107
GarretJT <garrettanzil@gmail.com>
2024-03-06 23:30:50 +0700

    ANMP Week2

--- app/src/main/java/com/example/adv160421097week2/MainActivity.kt
+++ app/src/main/java/com/example/adv160421097week2/MainActivity.kt
@@ -5,19 +5,25 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import com.example.adv160421097week2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        navController = (supportFragmentManager.findFragmentById(R.id.navHost) as NavHostFragment).navController
-
-//        NavigationUI.setupActionBarWithNavController(this,navController)
+        navController =
+            (supportFragmentManager.findFragmentById(R.id.navHost)
+                    as NavHostFragment).navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
+
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp()
